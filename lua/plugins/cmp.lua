470f8a933910b0fb110ba0222b11ec95b184b166
Dev Rajput <devrajput060901@gmail.com>
2024-07-16 18:39:10 +0530

    nvim-cmpo

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -10,16 +10,13 @@ return {
   config = function()
     -- See `:help cmp`
     local cmp = require("cmp")
-    local luasnip = require("luasnip")
-    luasnip.config.setup({})
 
     cmp.setup({
-      snippet = {
-        expand = function(args)
-          luasnip.lsp_expand(args.body)
-        end,
-      },
       completion = { completeopt = "menu,menuone,noinsert" },
+
+      window = {
+        completion = cmp.config.window.bordered(),
+      },
 
       -- For an understanding of why these mappings were
       -- chosen, you will need to read `:help ins-completion`
@@ -27,9 +24,9 @@ return {
       -- No, but seriously. Please read `:help ins-completion`, it is really good!
       mapping = cmp.mapping.preset.insert({
         -- Select the [n]ext item
-        ["<C-n>"] = cmp.mapping.select_next_item(),
+        -- ["<C-n>"] = cmp.mapping.select_next_item(),
         -- Select the [p]revious item
-        ["<C-p>"] = cmp.mapping.select_prev_item(),
+        --["<C-p>"] = cmp.mapping.select_prev_item(),
 
         -- Scroll the documentation window [b]ack / [f]orward
         ["<C-b>"] = cmp.mapping.scroll_docs(-4),
@@ -38,12 +35,18 @@ return {
         -- Accept ([y]es) the completion.
         --  This will auto-import if your LSP supports it.
         --  This will expand snippets if the LSP sent a snippet.
-        ["<C-y>"] = cmp.mapping.confirm({ select = true }),
+        -- ["<C-y>"] = cmp.mapping.confirm({ select = true }),
 
         -- If you prefer more traditional completion keymaps,
         -- you can uncomment the following lines
         ["<CR>"] = cmp.mapping.confirm({ select = true }),
-        ["<Tab>"] = cmp.mapping.select_next_item(),
+        ["<Tab>"] = function(fallback)
+          if cmp.visible() then
+            cmp.select_next_item()
+          else
+            fallback()
+          end
+        end,
         ["<S-Tab>"] = cmp.mapping.select_prev_item(),
 
         -- Manually trigger a completion from nvim-cmp.
@@ -57,26 +60,12 @@ return {
         --    $body
         --  end
         --
-        -- <c-l> will move you to the right of each of the expansion locations.
-        -- <c-h> is similar, except moving you backwards.
-        ["<C-l>"] = cmp.mapping(function()
-          if luasnip.expand_or_locally_jumpable() then
-            luasnip.expand_or_jump()
-          end
-        end, { "i", "s" }),
-        ["<C-h>"] = cmp.mapping(function()
-          if luasnip.locally_jumpable(-1) then
-            luasnip.jump(-1)
-          end
-        end, { "i", "s" }),
-
-        -- For more advanced Luasnip keymaps (e.g. selecting choice nodes, expansion) see:
-        --    https://github.com/L3MON4D3/LuaSnip?tab=readme-ov-file#keymaps
       }),
       sources = {
         { name = "nvim_lsp" },
-        { name = "luasnip" },
         { name = "path" },
+        { name = "buffer" },
+        { name = "luasnip" },
       },
     })
   end,
